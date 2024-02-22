cf8c384bdced19a3553155a5554414e3c49968ed
marks261 <mark@chtlab.io>
2024-02-22 09:52:38 +0700

    update

---
+++ src/components/Homepage/Benefit.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "@/styles/Layout/benefit.module.scss"
+
+const Benefit = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.containerFirst}>
+      </div>
+    </div>
+  )
+}
+
+export default Benefit
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import Faqs from "@/components/Homepage/Faqs";
 import Campaign from "@/components/Homepage/Campaign";
 import AboutNOTT from "@/components/Homepage/AboutNOTT";
 import ProductsShowcase from "@/components/Homepage/ProductsShowcase";
+import Benefit from "@/components/Homepage/Benefit";
 
 export default function Home() {
   return (
@@ -39,6 +40,7 @@ export default function Home() {
         <Hero />
         <Campaign />
         <AboutNOTT />
+        <Benefit />
         <ProductsShowcase />
         <Faqs />
         <Footer />
