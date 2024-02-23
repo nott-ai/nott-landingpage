3d89741b8eb51cdc0f18e34f53ce996f1be6661b
marks261 <mark@chtlab.io>
2024-02-23 08:18:13 +0700

    update benefit

--- src/components/Homepage/Benefit.tsx
+++ src/components/Homepage/Benefit.tsx
@@ -12,7 +12,24 @@ const Benefit = () => {
         </div>
         <div className={styles.bottomBlock}>
           <img src='/images/banner-benefit1.svg' alt='' />
+          <div className={styles.benefitContent}>
+            <div className={styles.title}>
+              User benefit
+            </div>
+            <div className={styles.mainTitle}>
+              Empowering User-Controlled
+              Data with NOTT
+            </div>
+            <div className={styles.description}>
+              Discover the ongoing revolution in health tracking through NOTT's Device Drop Campaigns. These periodic initiatives are our commitment to making advanced health monitoring accessible to all.
+            </div>
+            <div className={styles.description}>
+              Anonymous Data Sharing Rewards: Users can opt to anonymously share their health data, contributing to community health advancements and receiving rewards in return.            </div>
+            <div className={styles.description}>
+              Contributing to Research: By sharing anonymized data, users can directly contribute to health research, aiding in the development of better health and wellness insights and solutions.            </div>
+          </div>
         </div>
+
       </div>
     </div>
   )
