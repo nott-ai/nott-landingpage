ece9db6c88bad30ad75a4a7548ddb9b98cdbbac0
marks261 <mark@chtlab.io>
2024-02-23 08:35:34 +0700

    fix bug scroll in header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -3,7 +3,7 @@ import { NAVIGATIONS } from "@/constants/header";
 import styles from "@/styles/Layout/header.module.scss";
 import Link from "next/link";
 import { Link as LinkScroll } from "react-scroll";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useTrans from "@/hooks/useTrans";
 import Modal from "react-modal";
 import { it } from "node:test";
@@ -35,54 +35,77 @@ const Header = () => {
   const trans: any = useTrans();
   const [modalIsOpen, setIsOpen] = useState(false);
   const { isDesktop } = useDeviceDetect();
+  const [isHidden, setIsHidden] = useState(false);
   function openModal() {
     setIsOpen(!modalIsOpen);
   }
 
-
-
   function closeModal() {
     setIsOpen(false);
   }
+
+  useEffect(() => {
+    let lastScrollTop = 0;
+
+    const handleScroll = () => {
+      const st = window.pageYOffset || document.documentElement.scrollTop;
+      const wrapper = document.querySelector(`.${styles.wrapper}`);
+
+      if (wrapper) {
+        if (st > lastScrollTop) {
+          wrapper.classList.add(styles.hidden);
+        } else {
+          wrapper.classList.remove(styles.hidden);
+        }
+      }
+
+      lastScrollTop = st <= 0 ? 0 : st;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <>
-      <div className={styles.wrapper}>
-        <div className={styles.container}>
-          <div className={styles.content}>
-            <Link href="/">
-              <img className={styles.logo} src="/images/logo.svg" alt="logo" />
-            </Link>
-            <div className={styles.desktop}>
-              <div className={styles.navigation}>
-                {NAVIGATIONS.map((item) => (
-                  <div key={item.id}>
-                    {item.scroll ? (
-                      <LinkScroll
-                        activeClass="active"
-                        style={{ cursor: "pointer" }}
-                        target={item.link}
-                        to={item.link}
-                        smooth={true}
-                        spy={true}
-                        offset={isDesktop ? -80 : -44}
-                        duration={500}
-                        key={item.id}
-                      >
-                        <div>{trans.header[item.name]}</div>
-                      </LinkScroll>
-                    ) : (
-                      <Link
-                        key={item.id}
-                        href={item.link}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <div>{trans.header[item.name]}</div>
-                      </Link>
-                    )}
-                  </div>
-                ))}
-                {/* <DropdownMenu<HTMLButtonElement>
+      <header className={`wrapper ${isHidden ? 'hidden' : ''}`}>
+        <div className={styles.wrapper}>
+          <div className={styles.container}>
+            <div className={styles.content}>
+              <Link href="/">
+                <img className={styles.logo} src="/images/logo.svg" alt="logo" />
+              </Link>
+              <div className={styles.desktop}>
+                <div className={styles.navigation}>
+                  {NAVIGATIONS.map((item) => (
+                    <div key={item.id}>
+                      {item.scroll ? (
+                        <LinkScroll
+                          activeClass="active"
+                          style={{ cursor: "pointer" }}
+                          target={item.link}
+                          to={item.link}
+                          smooth={true}
+                          spy={true}
+                          offset={isDesktop ? -80 : -44}
+                          duration={500}
+                          key={item.id}
+                        >
+                          <div>{trans.header[item.name]}</div>
+                        </LinkScroll>
+                      ) : (
+                        <Link
+                          key={item.id}
+                          href={item.link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <div>{trans.header[item.name]}</div>
+                        </Link>
+                      )}
+                    </div>
+                  ))}
+                  {/* <DropdownMenu<HTMLButtonElement>
                 trigger={({
                   triggerRef,
                   isSelected,
@@ -123,41 +146,42 @@ const Header = () => {
                   ))}
                 </DropdownItemGroup>
               </DropdownMenu> */}
+                </div>
               </div>
-            </div>
 
-            <div className={styles.mobile} onClick={openModal}>
-              <MenuIcon />
+              <div className={styles.mobile} onClick={openModal}>
+                <MenuIcon />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        style={customStyles}
-        appElement={typeof window !== "undefined" ? document.body : undefined}
-      >
-        <div className={styles.navigationMobile}>
-          {NAVIGATIONS.map((item) => (
-            <LinkScroll
-              activeClass="active"
-              style={{ cursor: "pointer" }}
-              target={item.link}
-              to={item.link}
-              smooth={true}
-              spy={true}
-              offset={isDesktop ? -80 : -44}
-              duration={500}
-              key={item.id}
-              onClick={closeModal}
-              href={item.link}
-            >
-              <div>{trans.header[item.name]}</div>
-            </LinkScroll>
-          ))}
-        </div>
-      </Modal>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+          appElement={typeof window !== "undefined" ? document.body : undefined}
+        >
+          <div className={styles.navigationMobile}>
+            {NAVIGATIONS.map((item) => (
+              <LinkScroll
+                activeClass="active"
+                style={{ cursor: "pointer" }}
+                target={item.link}
+                to={item.link}
+                smooth={true}
+                spy={true}
+                offset={isDesktop ? -80 : -44}
+                duration={500}
+                key={item.id}
+                onClick={closeModal}
+                href={item.link}
+              >
+                <div>{trans.header[item.name]}</div>
+              </LinkScroll>
+            ))}
+          </div>
+        </Modal>
+      </header>
     </>
   );
 };
