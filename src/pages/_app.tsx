914e46884cf4feb3ce6dee1330d0f01505177600
paul nguyen <157973936+paulNottAi@users.noreply.github.com>
2024-03-04 15:07:46 +0700

    Update react hook cookie

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,13 @@ import { firebaseConfig } from "@/constants/firebase-config";
 import { getAnalytics } from "@firebase/analytics";
 import Header from "@/components/Layout/Header";
 import Footer from "@/components/Layout/Footer";
+import styles from "@/styles/Homepage/cookies-nott.module.scss";
+import "react-hook-consent/dist/styles/style.css";
+import {
+  ConsentBanner,
+  ConsentOptions,
+  ConsentProvider,
+} from "react-hook-consent";
 
 const app = initializeApp(firebaseConfig);
 if (app.name && typeof window !== "undefined") {
@@ -16,12 +23,27 @@ if (app.name && typeof window !== "undefined") {
 export const inter = Inter({ subsets: ["latin"] });
 
 export const orbitron = Orbitron({ subsets: ["latin"] });
+
+const consentOptions: ConsentOptions = {
+  services: [],
+  theme: "light",
+};
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${inter.className}`}>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </main>
+    <ConsentProvider options={consentOptions}>
+      <main className={`${inter.className}`}>
+        <ConsentBanner>
+          <>
+            Can we use cookies and external services according to our{" "}
+            <a href="test">privacy paasdasdaolicy</a> to improve the browsing
+            experience?
+          </>
+        </ConsentBanner>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </main>
+    </ConsentProvider>
   );
 }
