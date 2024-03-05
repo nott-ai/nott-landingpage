297b56a15cc6db9b3228f4396913f63477cd9f51
paul nguyen <157973936+paulNottAi@users.noreply.github.com>
2024-03-05 15:40:35 +0700

    Update product screen

---
+++ src/pages/product/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import ProductScreenSection from "@/components/ProductAndService/ProductScreenSection";
+
+const ProductPage = () => {
+  return (
+    <>
+      <h1>Product Page</h1>
+      <ProductScreenSection />
+    </>
+  );
+};
+
+export default ProductPage;
