a3a2638bbf652036c90bddfa9cb63c9b10c9178e
paul nguyen <157973936+paulNottAi@users.noreply.github.com>
2024-03-10 14:48:23 +0700

    Update thumbnail

--- src/pages/legal-and-compliance.tsx
+++ src/pages/legal-and-compliance.tsx
@@ -24,7 +24,10 @@ const LegalDisclaimerPage = () => {
           property="og:description"
           content="Ensuring Trust and Safety: NOTT's Vigilance in Legal Compliance and Ethical Standards in Health Innovation."
         />
-        <meta property="og:image" content="" />
+        <meta
+          property="og:image"
+          content="https://nott.ai/images/thumbnail.jpg"
+        />
         {/* Twitter Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="nott.ai" />
@@ -40,7 +43,10 @@ const LegalDisclaimerPage = () => {
           name="twitter:description"
           content="Ensuring Trust and Safety: NOTT's Vigilance in Legal Compliance and Ethical Standards in Health Innovation."
         />
-        <meta name="twitter:image" content="https://nott.ai/thumbnail.jpg" />
+        <meta
+          name="twitter:image"
+          content="https://nott.ai/images/thumbnail.jpg"
+        />
       </Head>
       <LegalDisclaimer />;
     </>
