10b9c1088ea98fc3c5bbb0439c2301c6ccbebefe
marks261 <mark@chtlab.io>
2024-04-08 14:04:47 +0700

    update thumbnail of support page

--- src/pages/support.tsx
+++ src/pages/support.tsx
@@ -5,35 +5,17 @@ const SupportPage = () => {
   return (
     <>
       <Head>
-        <title>NOTT Support</title>
-        <meta
-          name="description"
-          content="Join us on a transformative journey to optimal health with NOTT's advanced wellness ecosystem, integrating state-of-the-art healthcare technology and personalized well-being strategies designed for modern life."
-        />
-        <meta
-          name="keywords"
-          content="NOTT Foundation, HealthFi, Decentralized Physical Infrastructure Network, DePIN, AI Health Insights, Wellness Platform, Smart Wearables, Smart beds, Health Monitoring, Personalized Health, Health and Wellness Technology, Digital Health Ecosystem"
-        ></meta>
-        <link rel="canonical" href="https://nott.ai"></link>
-        <meta
-          property="og:title"
-          content="NOTT: Your Pathway to Enhanced Wellness"
-        />
-        <meta
-          property="og:description"
-          content="Join us on a transformative journey to optimal health with NOTT's advanced wellness ecosystem, integrating state-of-the-art healthcare technology and personalized well-being strategies designed for modern life."
-        />
-        <meta property="og:url" content="https://nott.ai" />
-        <meta
-          property="og:image"
-          content="https://nott.ai/images/thumbnail.webp"
-        />
-        <meta name="twitter:card" content="summary_large_image"></meta>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>Support - NOTT Foundation</title>
+        <meta name="description" content="Download now for Exclusive insights & Seamless connectivity!" />
+        <meta property="og:url" content="https://develop.nott-landingpage.pages.dev/support/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Support - NOTT Foundation" />
+        <meta property="og:description" content="Download now for Exclusive insights & Seamless connectivity!" />
+        <meta property="og:image" content="https://nott.ai/images/thumbnail.jpg" />
       </Head>
       <Support />
     </>
   );
 };
+
 export default SupportPage;
