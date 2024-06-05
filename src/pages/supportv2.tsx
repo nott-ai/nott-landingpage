0a009acb7e6579523751901968c1d05ce3c164eb
arthur <tantd041221@gmail.com>
2024-06-05 15:05:19 +0700

    update url

--- src/pages/supportv2.tsx
+++ src/pages/supportv2.tsx
@@ -6,28 +6,13 @@ const SupportV2 = () => {
   return (
     <>
       <Head>
-        <title>Support - NOTT Innovative Technology</title>
-        <meta
-          name="description"
-          content="Download now for Exclusive insights & Seamless connectivity!"
-        />
-        <meta
-          property="og:url"
-          content="https://develop.nott-landingpage.pages.dev/support/"
-        />
+      <title>Support - NOTT Innovative Technology</title>
+        <meta name="description" content="Download now for Exclusive insights & Seamless connectivity!" />
+        <meta property="og:url" content="https://nott.ai/supportv2/" />
         <meta property="og:type" content="website" />
-        <meta
-          property="og:title"
-          content="Support - NOTT Innovative Technology"
-        />
-        <meta
-          property="og:description"
-          content="Download now for Exclusive insights & Seamless connectivity!"
-        />
-        <meta
-          property="og:image"
-          content="https://nott.ai/images/thumbnail.jpg"
-        />
+        <meta property="og:title" content="Support - NOTT Innovative Technology" />
+        <meta property="og:description" content="Download now for Exclusive insights & Seamless connectivity!" />
+        <meta property="og:image" content="https://nott.ai/images/thumbnail.jpg" />
       </Head>
       <SupportV2Page />
     </>
