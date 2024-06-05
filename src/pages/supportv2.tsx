ca66b50bec18d0f2d8462bb1ff5accdf28caa574
arthur <tantd041221@gmail.com>
2024-06-05 14:57:17 +0700

    fixbug: add meta in support page v2

--- src/pages/supportv2.tsx
+++ src/pages/supportv2.tsx
@@ -1,8 +1,37 @@
 import SupportV2Page from "@/components/SupportV2";
+import Head from "next/head";
 import React from "react";
 
 const SupportV2 = () => {
-  return <SupportV2Page />;
+  return (
+    <>
+      <Head>
+        <title>Support - NOTT Innovative Technology</title>
+        <meta
+          name="description"
+          content="Download now for Exclusive insights & Seamless connectivity!"
+        />
+        <meta
+          property="og:url"
+          content="https://develop.nott-landingpage.pages.dev/support/"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Support - NOTT Innovative Technology"
+        />
+        <meta
+          property="og:description"
+          content="Download now for Exclusive insights & Seamless connectivity!"
+        />
+        <meta
+          property="og:image"
+          content="https://nott.ai/images/thumbnail.jpg"
+        />
+      </Head>
+      <SupportV2Page />
+    </>
+  );
 };
 
 export default SupportV2;
