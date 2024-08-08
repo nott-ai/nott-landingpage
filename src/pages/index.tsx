01aac7514aa396971b0e78aafd1d135f31352abc
“Jason” <jason@nott.ai>
2024-08-08 22:47:25 +0700

    update SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>NOTT Innovative Technology</title>
+        <title>NOTT Innovative Technology - AI-Powered Wellness DePIN Platform Shaping the Future of HealthFi</title>
         <meta
           name="description"
-          content="Join us on a transformative journey to optimal health with NOTT's advanced wellness ecosystem, integrating state-of-the-art healthcare technology and personalized well-being strategies designed for modern life."
+          content="Join us on a transformative journey to optimal health with NOTT's advanced wellness ecosystem, integrating state-of-the-art healthcare technology and personalized well-being strategies designed for modern life. NOTT is a pioneering HealthFi ecosystem, integrating advanced AI, DePIN (Decentralized Physical Infrastructure Network), and a unique rewards system to enhance personal health management and community wellness, supported by a comprehensive marketplace and decentralized governance."
         />
         <meta
           name="keywords"
@@ -19,28 +19,21 @@ export default function Home() {
         <link rel="canonical" href="https://nott.ai"></link>
         <meta
           property="og:title"
-          content="NOTT: Your Pathway to Enhanced Wellness"
+          content="NOTT Innovative Technology - AI-Powered Wellness DePIN Platform Shaping the Future of HealthFi"
         />
         <meta
           property="og:description"
-          content="Join us on a transformative journey to optimal health with NOTT's advanced wellness ecosystem, integrating state-of-the-art healthcare technology and personalized well-being strategies designed for modern life."
+          content="Join us on a transformative journey to optimal health with NOTT's advanced wellness ecosystem, integrating state-of-the-art healthcare technology and personalized well-being strategies designed for modern life. NOTT is a pioneering HealthFi ecosystem, integrating advanced AI, DePIN (Decentralized Physical Infrastructure Network), and a unique rewards system to enhance personal health management and community wellness, supported by a comprehensive marketplace and decentralized governance."
         />
         <meta property="og:url" content="https://nott.ai" />
-        <meta
-          property="og:image"
-          content="https://nott.ai/images/thumbnail.webp"
-        />
+        <meta property="og:image" content="https://nott.ai/images/thumbnail.webp" />
         <meta name="twitter:card" content="summary_large_image"></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         {isDesktop ? (
           <link rel="preload" href="/images/banner-main.webp" as="image" />
         ) : (
-          <link
-            rel="preload"
-            href="/images/banner-main-mobile.webp"
-            as="image"
-          />
+          <link rel="preload" href="/images/banner-main-mobile.webp" as="image" />
         )}
       </Head>
       <HomePage />
