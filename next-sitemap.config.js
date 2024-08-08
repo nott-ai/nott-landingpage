ab4485f8b2784b31028098bd43f2f63f351e931e
“Jason” <jason@nott.ai>
2024-08-08 23:21:23 +0700

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL,
-  generateRobotsTxt: true, // (optional)
-  // ...other options
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    additionalSitemaps: ["https://blog.nott.ai/sitemap.xml"],
+  },
 };
